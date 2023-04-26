54cb4572801ff87ca0d5e8161fb03d1efa8003cf
Kazuhide Matsushime <shiroiinutokuroineko@gmail.com>
2023-04-26 17:05:54 +0900

    HP

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-//スライダーの設定
-const slides = document.querySelectorAll(".slide");
+// 画像を右から左に流す
+const slides = document.querySelectorAll('.slide');
 let currentSlide = 0;
-const slideInterval = setInterval(nextSlide, 3000);
 
-function nextSlide() {
+// 初期スライド位置の設定
+slides[currentSlide].classList.add('active');
+slides[currentSlide].style.left = '0';
+
+// 3秒ごとにスライドを移動する
+setInterval(moveSlides, 3000);
+
+function moveSlides() {
     // 現在のスライドを非アクティブにし、次のスライドをアクティブにする
-    slides[currentSlide].classList.remove("active");
+    slides[currentSlide].classList.remove('active');
     currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add("active");
+    slides[currentSlide].classList.add('active');
 
-    // 最後のスライドを表示している場合、最初のスライドに戻す
-    if (currentSlide === 0) {
-        slides[slides.length - 1].classList.remove("active");
-        slides[1].classList.remove("active");
-        slides[0].classList.add("active");
-    } else {
-        slides[currentSlide - 1].classList.remove("active");
-        slides[currentSlide + 1].classList.remove("active");
-        slides[currentSlide].classList.add("active");
-    }
+    // スライドの位置を変更する
+    slides.forEach((slide, index) => {
+        if (index === currentSlide) {
+            slide.style.left = '0';
+        } else if (index === (currentSlide + 1) % slides.length) {
+            slide.style.left = '0%';
+        } else {
+            slide.style.left = '-100%';
+        }
+    });
 }
